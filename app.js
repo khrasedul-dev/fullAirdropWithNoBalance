b214f69c63c169387d81711fa53fdaefb4458311
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-02-25 14:47:29 +0600

    Update app.js

--- app.js
+++ app.js
@@ -259,7 +259,7 @@ bot.start((ctx) => {
 			const r = data[0].referral_count
 
 
-			ctx.telegram.sendMessage(ctx.from.id, "Account Info:\n\nName - " + ctx.from.first_name + "\nWallet Address - " + wallet + "\nReferral Users - " + r + " \n\nReferral Link - \n\n (Tap to copy your link)**`https://t.me/" + ctx.botInfo.username + "?start=" + ctx.from.id + "`**\n\nShare your referral links with your friends on Telegram, WhatsApp, Facebook, and Twitter and tell them about this airdrop. When they join this contest through your referral link, your referral Users count . We will award 0.5 bnb worth of tokens each to 150 persons with the highest number of referrals. Good luck", {
+			ctx.telegram.sendMessage(ctx.from.id, "Account Info:\n\nName - " + ctx.from.first_name + "\nWallet Address - " + wallet + "\nReferral Users - " + r + " \n\nReferral Link - \n\n (Tap to copy your link) \n\n **`https://t.me/" + ctx.botInfo.username + "?start=" + ctx.from.id + "`**\n\nShare your referral links with your friends on Telegram, WhatsApp, Facebook, and Twitter and tell them about this airdrop. When they join this contest through your referral link, your referral Users count . We will award 0.5 bnb worth of tokens each to 150 persons with the highest number of referrals. Good luck", {
 				reply_markup: {
 					inline_keyboard: [
 						[{
